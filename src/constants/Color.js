a2dddb522eec394e825841733a558c4acebf5309
kdilot <kdilot@gmail.com>
2019-12-21 15:55:36 +0900

    color

--- src/constants/Color.js
+++ src/constants/Color.js
@@ -1,10 +1,9 @@
 const rozeusColor = '#d82027';
-
 const basicColor = '#2196F3';
-
 const whiteColor = 'white';
-
 const dividerDarkColor = '#ADB0B2';
 const dividerLightColor = '#ede8e8';
+const plusColor = '#4f55e2';
+const minusColor = '#fc7177';
 
-export { rozeusColor, basicColor, whiteColor, dividerDarkColor, dividerLightColor };
+export { rozeusColor, basicColor, whiteColor, dividerDarkColor, dividerLightColor, plusColor, minusColor };

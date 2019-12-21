3deda1e28ebf250bdfe629047356f8a020d2f7d3
kdilot <kdilot@gmail.com>
2019-12-22 01:14:20 +0900

    WalletHistoryComponent state

--- src/components/WalletHistoryComponent/index.js
+++ src/components/WalletHistoryComponent/index.js
@@ -20,22 +20,22 @@ export default class WalletHistoryComponent extends Component {
         super(props);
 
         this.state = {
-            nickname: props.address,
+            name: props.address,
         };
     }
 
     onChange = text => {
-        this.setState({ nickname: text });
+        this.setState({ name: text });
     };
 
     render() {
         const { send, status, date, value } = this.props;
-        const { nickname } = this.state;
+        const { name } = this.state;
         return (
             <CardView cardElevation={5} cornerRadius={10} style={styles.cardLayout}>
                 <View style={[styles.addressLayout, styles.borderColor(send ? plusColor : minusColor)]}>
                     <View style={styles.addressTextfield}>
-                        <TextInput value={nickname} keyboardType={'default'} onChangeText={text => this.onChange(text)} />
+                        <TextInput value={name} keyboardType={'default'} onChangeText={text => this.onChange(text)} />
                     </View>
                 </View>
                 <View style={styles.addressButtonLayout}>
@@ -67,7 +67,7 @@ export default class WalletHistoryComponent extends Component {
 
 WalletHistoryComponent.proptypes = {
     address: PropTypes.string,
-    nickname: PropTypes.string,
+    name: PropTypes.string,
     status: PropTypes.string,
     date: PropTypes.string,
     value: PropTypes.number,

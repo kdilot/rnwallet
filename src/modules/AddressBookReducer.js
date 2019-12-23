b06983a4496f82a62dca6acafb2eb077ec4f1d3b
kdilot <kdilotdev@gmail.com>
2019-12-23 13:48:07 +0900

    AddressBook Redux

---
+++ src/modules/AddressBookReducer.js
@@ -0,0 +1,30 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+import AddressBookList from 'asset/data/addressbook.json';
+
+//  Actions Type
+export const GETADDRESS = 'locale/GETADDRESS';
+export const SETADDRESS = 'locale/SETADDRESS';
+
+//  Actions
+export const getAddressBook = createAction(GETADDRESS);
+export const setAddressBook = createAction(SETADDRESS);
+
+// Default State
+const initialState = {
+    list: [],
+};
+
+export default handleActions(
+    {
+        [GETADDRESS]: (state, action) =>
+            produce(state, draft => {
+                draft.list = AddressBookList;
+            }),
+        [SETADDRESS]: (state, action) =>
+            produce(state, draft => {
+                console.log(`[SET ADDRESSBOOK]`, action.payload);
+            }),
+    },
+    initialState,
+);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import AddressBookReducer from './AddressBookReducer.js';
 import LocaleReducer from './LocaleReducer.js';
 
 export default combineReducers({
+    AddressBookReducer,
     LocaleReducer,
 });

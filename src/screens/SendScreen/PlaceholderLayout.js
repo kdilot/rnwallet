ac23bf65ee5b5a5305f01fbe3c355d3fcdbe9303
kdilot <kdilotdev@gmail.com>
2019-12-30 14:51:28 +0900

    EtherChain Gas Price API

---
+++ src/screens/SendScreen/PlaceholderLayout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Placeholder, PlaceholderLine, Shine } from 'rn-placeholder';
+
+export default class Placeholderlayout extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Placeholder Animation={Shine}>
+                    <PlaceholderLine style={styles.lineLayout} height={45} />
+                </Placeholder>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    lineLayout: {
+        borderRadius: 10,
+    },
+});

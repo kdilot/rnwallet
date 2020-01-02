edf724510948b5de2f3d2f890161ca036ad59322
test-rozeus <rozeus1121@gmail.com>
2020-01-02 17:45:37 +0900

    - decimal: create floorDecimalPoint

--- src/api/WalletHistory/decimal.js
+++ src/api/WalletHistory/decimal.js
@@ -1,17 +1,30 @@
-const bigdecimal = require("bigdecimal");
+const bigdecimal = require('bigdecimal');
 
 export const div = (a, b) => {
-    let decimalA = new bigdecimal.BigDecimal(String(a)).setScale(
-        8,
-        bigdecimal.BigDecimal.ROUND_HALF_UP
-    );
-    let decimalB = new bigdecimal.BigDecimal(String(b)).setScale(
-        8,
-        bigdecimal.BigDecimal.ROUND_HALF_UP
-    );
+    let decimalA = new bigdecimal.BigDecimal(String(a)).setScale(8, bigdecimal.BigDecimal.ROUND_HALF_UP);
+    let decimalB = new bigdecimal.BigDecimal(String(b)).setScale(8, bigdecimal.BigDecimal.ROUND_HALF_UP);
 
     return decimalA
         .divide(decimalB)
         .setScale(8, bigdecimal.BigDecimal.ROUND_HALF_UP)
         .toString();
+};
+
+export const floorDecimalPoint = (num, place) => {
+    if (!Number(num) || (!Number(place) && place !== 0)) {
+        return num;
+    }
+
+    if (place > 9) {
+        place = 9;
+    } else if (place < -9) {
+        place = -9;
+    }
+
+    let commaIndex = num.indexOf('.');
+    if (commaIndex === -1) {
+        return num;
+    }
+
+    return num.substring(0, commaIndex + place + 1);
 };

beacc9992737d96f0198b8d558bfe6703be050ec
kdilot <kdilotdev@gmail.com>
2020-01-03 15:28:00 +0900

    etherjs

--- src/api/etherjs.js
+++ src/api/etherjs.js
@@ -13,3 +13,27 @@ export const fromMnemonic = mnemonic => {
 export const entropyToMnemonic = () => {
     return ethers.utils.HDNode.entropyToMnemonic(ethers.utils.randomBytes(16), ethers.wordlists.en);
 };
+
+export const getDefaultProvider = unit => {
+    return ethers.getDefaultProvider(unit);
+};
+
+export const etherWallet = (privateKey, provider) => {
+    return new ethers.Wallet(privateKey, provider);
+};
+
+export const contract = (contractAddress, abi, ethWallet) => {
+    return new ethers.Contract(contractAddress, abi, ethWallet);
+};
+
+export const parseUnits = (value, type) => {
+    return ethers.utils.parseUnits(value, type);
+};
+
+export const bigNumberify = value => {
+    return ethers.utils.bigNumberify(value);
+};
+
+export const parseEther = value => {
+    return ethers.utils.parseEther(value);
+};

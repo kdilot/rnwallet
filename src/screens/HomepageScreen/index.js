ed9bdd6f8ce82dd4891d7cdded6ac95b1bba5c55
kdilot <kdilotdev@gmail.com>
2020-01-14 14:04:17 +0900

    HomepageScreen

--- src/screens/HomepageScreen/index.js
+++ src/screens/HomepageScreen/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, View, Text } from 'react-native';
 import { WebView } from 'react-native-webview';
 import styles from './styles';
 
 const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
 
 export default class HomePageScreen extends Component {
     render() {
-        return <WebView source={{ uri: 'https://rozeus.io/' }} style={[styles.container, { width: WIDTH }]} />;
+        return (
+            <WebView
+                source={{ uri: 'https://rozeus.io/' }}
+                style={[styles.container, { width: WIDTH }]}
+                startInLoadingState={true}
+                renderLoading={() => (
+                    <View style={[styles.loadingLayout, { width: WIDTH, height: HEIGHT }]}>
+                        <Text>Loading</Text>
+                    </View>
+                )}
+            />
+        );
     }
 }
--- src/screens/HomepageScreen/styles.js
+++ src/screens/HomepageScreen/styles.js
@@ -1,7 +1,9 @@
-export default {
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-};
+    loadingLayout: { zIndex: 1000, justifyContent: 'center', alignItems: 'center' },
+});

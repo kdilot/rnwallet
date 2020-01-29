23542147de727d0d70d6fdf0d3660b5e62877c20
kdilot <kdilotdev@gmail.com>
2020-01-29 17:52:26 +0900

    Notice sample

--- src/screens/NoticeScreen/index.js
+++ src/screens/NoticeScreen/index.js
@@ -1,13 +1,75 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import React, { PureComponent } from 'react';
+import { Text, View, SafeAreaView, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import S from './styles';
 
-export default class NoticeScreen extends Component {
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
+
+const arr = new Array(10);
+
+class ListLayout extends PureComponent {
     render() {
+        const { navigation } = this.props.screenProps;
         return (
-            <View style={styles.container}>
-                <Text> 서비스 준비중 </Text>
-            </View>
+            <SafeAreaView>
+                <FlatList
+                    data={arr}
+                    renderItem={({ item, index }) => (
+                        <TouchableOpacity style={S.NoticeListView} onPress={() => navigation.push('Notice', { selected: index })}>
+                            <Text>공지사항 제목</Text>
+                        </TouchableOpacity>
+                    )}
+                    ItemSeparatorComponent={() => <View style={S.DividerView} />}
+                    ListFooterComponent={() => arr && <View style={S.DividerView} />}
+                    keyExtractor={(item, index) => index.toString()}
+                />
+            </SafeAreaView>
+        );
+    }
+}
+
+const BottomMenuTab = createAppContainer(
+    createMaterialTopTabNavigator(
+        {
+            Notice: {
+                screen: ListLayout,
+                navigationOptions: {
+                    title: '공지',
+                },
+            },
+            Qna: {
+                screen: ListLayout,
+                navigationOptions: {
+                    title: 'Q&A',
+                },
+            },
+        },
+        {
+            animationEnabled: true,
+            swipeEnabled: true,
+            tabBarPosition: 'top',
+            initialRouteName: 'Notice',
+            tabBarOptions: {
+                showLabel: true,
+            },
+        },
+    ),
+);
+
+export default class NoticeScreen extends PureComponent {
+    render() {
+        const { params } = this.props.navigation.state;
+        return params ? (
+            <SafeAreaView>
+                <View style={S.NoticeHeaderView}>
+                    <Text>공지사항 제목</Text>
+                </View>
+                <ScrollView style={S.NoticeContentView}>
+                    <Text>공지사항 내용</Text>
+                </ScrollView>
+            </SafeAreaView>
+        ) : (
+            <BottomMenuTab screenProps={{ navigation: this.props.navigation }} />
         );
     }
 }
--- src/screens/NoticeScreen/styles.js
+++ src/screens/NoticeScreen/styles.js
@@ -1,7 +1,19 @@
-export default {
-    container: {
-        flex: 1,
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    NoticeHeaderView: {
+        height: 70,
+        borderBottomWidth: 1,
+        padding: 20,
         justifyContent: 'center',
-        alignItems: 'center',
     },
-};
+    NoticeContentView: {
+        padding: 20,
+    },
+    NoticeListView: {
+        padding: 20,
+    },
+    DividerView: {
+        borderWidth: 0.5,
+    },
+});

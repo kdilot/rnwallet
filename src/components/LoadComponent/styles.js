026d172b1e80e45cf58b046e24fede8ba79bb4b3
kdilot <kdilotdev@gmail.com>
2020-01-31 09:55:18 +0900

    LoadComponent

---
+++ src/components/LoadComponent/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    IsEmptyView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});

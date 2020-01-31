65173cb618073192c933f3c50b4db108da856082
kdilot <kdilotdev@gmail.com>
2020-01-31 15:00:59 +0900

    AddressBookListComponent toast

--- src/components/AddressBookListComponent/index.js
+++ src/components/AddressBookListComponent/index.js
@@ -11,73 +11,75 @@ import { USER_ETH_ADDRESS } from 'constants/Global';
 import * as addressActions from 'modules/AddressBookReducer';
 
 class AddressBookListComponent extends Component {
-	onCopy = text => {
-		const { toast } = this.props;
-		const { lang } = this.props.navigation.getScreenProps('locale');
-		Clipboard.setString(text);
-		toast.showToast(lang.copy);
-	};
+    onCopy = text => {
+        const { toast } = this.props;
+        const { lang } = this.props.navigation.getScreenProps('locale');
+        Clipboard.setString(text);
+        toast.showToast(lang.copy);
+    };
 
-	onSend = address => {
-		const isRoz = true;
-		this.props.navigation.navigate('Send', { address, send: true, coin: isRoz ? 'ROZ' : 'ETH' });
-	};
+    onSend = address => {
+        const isRoz = true;
+        this.props.navigation.navigate('Send', { address, send: true, coin: isRoz ? 'ROZ' : 'ETH' });
+    };
 
-	onDelete = async address => {
-		const { AddressAction } = this.props;
+    onDelete = async address => {
+        const { AddressAction, toast } = this.props;
+        const { lang } = this.props.navigation.getScreenProps('locale');
 
-		let addressBookMap = await deleteAddressBook(USER_ETH_ADDRESS, address);
-		if (!addressBookMap) {
-			return;
-		}
-		await AddressAction.setAddressBook(addressBookMap);
-	};
+        let addressBookMap = await deleteAddressBook(USER_ETH_ADDRESS, address);
+        if (!addressBookMap) {
+            return;
+        }
+        await AddressAction.setAddressBook(addressBookMap);
+        toast.showToast(lang.deleteMsg);
+    };
 
-	render() {
-		const { item } = this.props;
-		return (
-			<View style={S.ContainerView}>
-				<View style={S.TextView}>
-					<Text numberOfLines={1} ellipsizeMode="tail">
-						{item.nickname}
-					</Text>
-				</View>
-				<View style={S.ButtonGroupView}>
-					<TouchableOpacity
-						style={S.ButtonView}
-						onPress={() => {
-							this.onDelete(item.address);
-						}}>
-						<Feather name="trash" size={20} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={S.ButtonView}
-						onPress={() => {
-							this.onCopy(item.address);
-						}}>
-						<Feather name="copy" size={20} />
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={S.ButtonView}
-						onPress={() => {
-							this.onSend(item.address);
-						}}>
-						<Feather name="send" size={20} />
-					</TouchableOpacity>
-				</View>
-			</View>
-		);
-	}
+    render() {
+        const { item } = this.props;
+        return (
+            <View style={S.ContainerView}>
+                <View style={S.TextView}>
+                    <Text numberOfLines={1} ellipsizeMode="tail">
+                        {item.nickname}
+                    </Text>
+                </View>
+                <View style={S.ButtonGroupView}>
+                    <TouchableOpacity
+                        style={S.ButtonView}
+                        onPress={() => {
+                            this.onDelete(item.address);
+                        }}>
+                        <Feather name="trash" size={20} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={S.ButtonView}
+                        onPress={() => {
+                            this.onCopy(item.address);
+                        }}>
+                        <Feather name="copy" size={20} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={S.ButtonView}
+                        onPress={() => {
+                            this.onSend(item.address);
+                        }}>
+                        <Feather name="send" size={20} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 AddressBookListComponent.proptypes = {
-	addressBookList: PropTypes.object,
-	onActive: PropTypes.func,
+    addressBookList: PropTypes.object,
+    onActive: PropTypes.func,
 };
 
 export default connect(
-	state => ({}),
-	dispatch => ({
-		AddressAction: bindActionCreators(addressActions, dispatch),
-	}),
+    state => ({}),
+    dispatch => ({
+        AddressAction: bindActionCreators(addressActions, dispatch),
+    }),
 )(AddressBookListComponent);

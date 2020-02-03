778492d93b3c421e6cdd8812b3f36eb30e3271b7
kdilot <kdilotdev@gmail.com>
2020-02-03 09:48:40 +0900

    Icon

--- src/components/NoticeComponent/index.js
+++ src/components/NoticeComponent/index.js
@@ -1,8 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { Component } from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { IconComponent } from 'components';
 import styles from './styles';
-import ethPng from 'asset/icon/eth.png';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class NoticeComponent extends Component {
     static defaultProps = {
@@ -27,8 +27,8 @@ export default class NoticeComponent extends Component {
                         <Text style={styles.inDt}>{this.props.notice.inDt}</Text>
                     </View>
                     <View style={styles.btnBox}>
-                        <ImageBackground style={{ width: 20, height: 20 }} source={ethPng} />
-                        <ImageBackground style={{ width: 20, height: 20, marginLeft: 10 }} source={ethPng} />
+                        <IconComponent name={'eth'} size={20} />
+                        <IconComponent name={'eth'} size={20} style={{ marginLeft: 10 }} />
                     </View>
                 </TouchableOpacity>
             </View>
